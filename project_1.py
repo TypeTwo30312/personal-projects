050de9843ec4fa8fe1ed430f7ebce950c970260b
Tomáš Vamberský <tomas.vambersky@protonmail.com>
2025-04-22 21:12:57 +0200

    added login functionality

--- project_1.py
+++ project_1.py
@@ -1,1 +1,25 @@
-print('hello')
+"""
+
+projekt_1.py: první projekt do Engeto Online Python Akademie
+
+author: Tomáš Vamberský
+
+email: tomas.vambersky@protonmail.com
+
+discord:
+
+"""
+users = {
+    "bob": "123",
+    "ann": "pass123",
+    "mike": "password123",
+    "liz": "pass123"
+}
+
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+if username in users and users[username] == password:
+    print("Login successful!")
+else:
+    print("Invalid username or password.")
