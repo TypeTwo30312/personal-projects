3beab3332026bec558667b88c7a811a539e28583
TypeTwo30312 <tomas.vambersky@protonmail.com>
2025-04-23 11:06:18 +0200

    added function that returns town data

--- project_3/project_3.py
+++ project_3/project_3.py
@@ -23,7 +23,6 @@ def find_district_link(district):
     parsed_page = BeautifulSoup(page.text, features="html.parser")
     district_dict = {}
     rows = parsed_page.find_all("tr")
-    print(rows)
     for row in rows:
         cells = row.find_all("td")
         if len(cells) >= 4:
@@ -33,8 +32,36 @@ def find_district_link(district):
                 relative_link = html.unescape(link_tag["href"]) # escaping the "&amp;"
                 full_link = base_url + relative_link
                 district_dict[district_name] = full_link
-    print(district_dict[district])
     return district_dict[district]
 
-find_district_link("Praha")
-### first css selector table.table:nth-child(7) > tbody:nth-child(1) > tr:nth-child(3)
+def get_town_links(district_link):
+    """Return a list of dicts with town code, name, and result page link from the given district link."""
+    base_url = "https://www.volby.cz/pls/ps2017nss/"
+    page = get(district_link)
+    soup = BeautifulSoup(page.text, "html.parser")
+
+    town_list = []
+
+    rows = soup.find_all("tr")
+
+    for row in rows:
+        cells = row.find_all("td")
+        if len(cells) >= 3:
+            code = cells[0].text.strip()                    # first <td>: town code
+            name = cells[1].text.strip()                    # second <td>: town name
+            link_tag = cells[2].find("a")                   # third <td>: link to result page (adjust index if needed)
+
+            if link_tag:
+                relative_link = html.unescape(link_tag["href"])
+                full_link = base_url + relative_link
+                town_list.append({
+                    "code": code,
+                    "name": name,
+                    "link": full_link
+                })
+
+    return town_list
+
+district_link = find_district_link("Ostrava-město")
+towns = get_town_links(district_link)
+print(towns)
